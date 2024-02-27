b87f641cd50768966d96d1d0549be8a6533c11d7
planifyit.com <contact@planifyit.com>
2024-02-27 17:17:22 +0100

    Update BarChartRaceWidget.js

--- BarChartRaceWidget.js
+++ BarChartRaceWidget.js
@@ -156,21 +156,27 @@ async _updateData(dataBinding) {
     console.log("Data Binding Received:", dataBinding);
     if (!this._ready) {
         console.log("D3 not ready, storing data for later");
-        // Ensure _pendingData stores the entire dataBinding object
-        this._pendingData = dataBinding; 
+        this._pendingData = dataBinding;
         return;
     }
 
     try {
         if (dataBinding && dataBinding.data) {
-            // Wrap the transformation logic in a try-catch block
             console.log("Original Data:", dataBinding.data);
+
+            // Validate each data point
+            dataBinding.data.forEach(d => {
+                if (typeof d.measures_0.raw !== 'number') {
+                    console.error("Data integrity issue found:", d);
+                }
+            });
+
             const transformedData = this.transformDataForBarChartRace(dataBinding.data);
             console.log("Transformed Data:", transformedData);
             if (!transformedData || transformedData.length === 0) {
                 throw new Error("Transformed data is empty or invalid.");
             }
-            this.currentData = transformedData; 
+            this.currentData = transformedData;
             console.log("Transformed Data for Rendering:", transformedData);
             this._maybeRenderChart();
         } else {
@@ -178,9 +184,9 @@ async _updateData(dataBinding) {
         }
     } catch (error) {
         console.error("Error processing data for rendering:", error);
-        // Handle the error as needed (e.g., display an error message to the user)
     }
 }
+
 
 
  _maybeRenderChart() {
